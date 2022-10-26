c03bd674ff258b554b3acda377559d1add86cfa7
ebrulf <konstantyd@o2.pl>
2022-10-26 12:06:27 +0200

    Dodaj pliki projektów.

---
+++ Adapter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Symulacja biblioteki graficznej o metodah:
+            //drawLine(x1, x2, y1, y2)
+            //drawCircle(x, y, r)
+            //drawRectangle(x, y, width, height)
+            //Zaimplementuj adapter z metodą drawCar i drawRabbit
+            //(abstrakcja), nie trzeba aż tak szczegółowo 
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Composite/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kompozyt o atrybucie
+            //PRICE:float
+            //plus operacja dodawania wszystkich cen w drzewie
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
