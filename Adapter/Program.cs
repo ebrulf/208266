b63c1e50e4e3d283d2a36101d976f02a866fef00
ebrulf <konstantyd@o2.pl>
2022-10-26 12:26:20 +0200

    szczątki zrobione w obu projektach

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -2,6 +2,35 @@
 
 namespace Adapter
 {
+    public interface Adaptee
+    {
+        void drawLine(int x1, int x2, int y1, int y2);
+        void drawCircle(int x, int y, int r);
+        void drawRectangle(int x, int y, int width, int hieght);
+    }
+    public class Adapter : Adaptee
+    {
+        public void drawLine(int x1, int x2, int y1, int y2)
+        {
+            Console.WriteLine("Narysowano linię od (" + x1 + ", " + y1 + ") do (" + x2", " + y2 + ")");
+        }
+        public void drawCircle(int x, int y, int r)
+        {
+            //itp.
+        }
+        public void drawRectangle(int x, int y, int width, int hieght)
+        {
+
+        }
+        public void drawCar(int x, int y, int width, int height)
+        {
+            //1 prostokąt + 2 koła
+        }
+        public void drawRabbit(int x, int y, int czub)
+        {
+            //dwie kulki plus uszy - 2x Circle + 3xdrawLine
+        }
+    }
     class Program
     {
         static void Main(string[] args)
--- Composite/Program.cs
+++ Composite/Program.cs
@@ -2,6 +2,18 @@
 
 namespace Composite
 {
+    abstract class Component
+    {
+
+    }
+    class Leaf : Component
+    {
+
+    }
+    class Composite : Component
+    {
+
+    }
     class Program
     {
         static void Main(string[] args)
@@ -9,6 +21,7 @@ namespace Composite
             //Kompozyt o atrybucie
             //PRICE:float
             //plus operacja dodawania wszystkich cen w drzewie
+            //prezka: https://onedrive.live.com/?authkey=%21ADQ4tJHklEGVoRA&cid=0EE2BAC1E3702B44&id=EE2BAC1E3702B44%21213767&parId=root&o=OneUp
             Console.WriteLine("Hello World!");
         }
     }
